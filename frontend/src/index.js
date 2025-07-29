4a25660b5a4f21c72d4e08582774a78c0accdaec
AreeshaUmar91 <areeshaumar44@gmail.com>
2025-07-29 10:42:52 -0700

    do changes in frontend part

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter> {/* ✅ Wrap App in BrowserRouter */}
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
-)
+);
